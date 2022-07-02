46d9b0cd438b22edfd0010b1176c224d9dd0f8f0
dennis <dennis.teles@gmail.com>
2022-07-02 16:29:34 -0300

    Subtotal & Total methods

--- src/main/java/com/example/project/entities/OrderItem.java
+++ src/main/java/com/example/project/entities/OrderItem.java
@@ -74,6 +74,10 @@ public class OrderItem implements Serializable{
         return result;
     }
 
+    public Double getSubTotal() {
+        return price * quantity;
+    }
+
     @Override
     public boolean equals(Object obj) {
         if (this == obj)
