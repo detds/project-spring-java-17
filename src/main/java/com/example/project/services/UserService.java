62c35ee97c368647306df8c348e9e512046d72e7
dennis <dennis.teles@gmail.com>
2022-07-02 17:13:40 -0300

    User insert

--- src/main/java/com/example/project/services/UserService.java
+++ src/main/java/com/example/project/services/UserService.java
@@ -21,4 +21,8 @@ public class UserService {
     public User findById(Long id) {
        return repository.findById(id).get();
     }
+
+    public User insert(User obj) {
+        return repository.save(obj);
+    }
 }
